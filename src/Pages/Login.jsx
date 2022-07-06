4a3c3df241ef5481e8a7573f1883114c6e7c3b28
cae <eduardokae01@hotmail.com>
2022-07-06 17:03:58 -0300

    feat: Login functions

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { getItemLocalStorage, setItemLocalStorage } from '../Utils';
+import loginSchema from '../validation';
+
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  const navigate = useNavigate();
+  const tokenTest = 1;
+
+  const validation = () => {
+      const { error } = loginSchema.validate({ email, password });  
+      return error? setDisabled(true) : setDisabled(false);
+  };
+
+  const handleChange = ({ target: { name, value } }) => {
+    name === 'email'? setEmail(value) : setPassword(value);
+    return validation();
+  };
+
+  const emailAllreadyExist = () => {
+    const emailFromStorage = getItemLocalStorage('user');
+    return emailFromStorage === email ? navigate('/comidas') : true;
+  };
+
+  const handleClick = () => {
+    emailAllreadyExist();
+    setItemLocalStorage('mealsToken', tokenTest);
+    setItemLocalStorage('cocktailsToken', tokenTest);
+    setItemLocalStorage('user', { email });
+    setItemLocalStorage('doneRecipes', []);
+    setItemLocalStorage('favoriteRecipes', []);
+    setItemLocalStorage('inProgressRecipes', []);
+    return navigate('/comidas');
+  };
+
   return(
     <Form>
       <h1>
@@ -12,6 +48,9 @@ export default function Login() {
           type="email"
           placeholder="Email"
           data-testid="email-input"
+          name="email"
+          value={ email }
+          onChange={ handleChange }
         />
       </Form.Group>
       <Form.Group className="mb-3" controlId="Password">
@@ -19,13 +58,17 @@ export default function Login() {
           type="password"
           placeholder="Senha"
           data-testid="password-input"
+          name="password"
+          value={ password }
+          onChange={ handleChange }
         />
       </Form.Group>
       <Button
         variant="primary"
         type="button"
         data-testid="login-submit-btn"
-        disabled
+        disabled={ disabled }
+        onClick={ handleClick }
       >
         Entrar
       </Button>
