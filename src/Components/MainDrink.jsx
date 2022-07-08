846f618d038ae790dad9fc4ade8b790f18cbb026
cae <eduardokae01@hotmail.com>
2022-07-08 13:34:03 -0300

    feat: render first drink recipes

--- src/Components/MainDrink.jsx
+++ src/Components/MainDrink.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { getFirstDrinkRecipes } from '../Helpers/apiConexions';
 
 export default function MainDrink() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    async function getFirstRecipes() {
+      const data = await getFirstDrinkRecipes();
+      setRecipes(data);
+    }
+    getFirstRecipes();
+  }, []);
+
   return (
-    <h1>drinks</h1>
-  )
+    recipes.map((recipe) => {
+      return (
+        <section>
+          <img src={ recipe.strDrinkThumb } alt={ recipe.strDrink } width="125px" />
+          <h6>{ recipe.strDrink }</h6>
+        </section>
+
+      );
+    })
+  );
 };
