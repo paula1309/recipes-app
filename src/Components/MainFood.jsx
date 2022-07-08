6ee2ddefb514332523d4dc6433e925a15e8e4bd0
cae <eduardokae01@hotmail.com>
2022-07-08 13:17:36 -0300

    feat: render first food recipes

--- src/Components/MainFood.jsx
+++ src/Components/MainFood.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { getFirstFoodRecipes } from "../Helpers/apiConexions";
 
 export default function MainFood() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    async function getFoodRecipes() {
+      const data = await getFirstFoodRecipes();
+      setRecipes(data);
+    }
+    getFoodRecipes();
+  }, []);
+
+  console.log(recipes);
+
   return (
-    <h1>Main food</h1>
+    recipes.map((recipe) => {
+      return (
+        <section key={ recipe.idMeal }>
+          <img src={ recipe.strMealThumb } alt={ recipe.Meal } width="125px" />
+          <h6>{ recipe.strMeal }</h6>
+        </section>
+      )
+    })
   )
 };
