bf470d43978edef7315020fd14d24a75978c95d4
cae <eduardokae01@hotmail.com>
2022-07-08 13:23:12 -0300

    feat: get first drink recipes and fix lint

--- src/Helpers/apiConexions.js
+++ src/Helpers/apiConexions.js
@@ -1,23 +1,28 @@
 import axios from 'axios';
 
 const getFoodByIngredient = async (ingredient) => {
-  const { data: {meals} } = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+  const { data: { meals } } = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
   return meals;
 };
 
 const getFoodByName = async (name) => {
-  const { data: {meals} } = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
+  const { data: { meals } } = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
   return meals;
 };
 
 const getFoodByFirstName = async (firstName) => {
-  const { data: {meals} } = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?f=${firstName}`);
+  const { data: { meals } } = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?f=${firstName}`);
   return meals;
 };
 
 const getFirstFoodRecipes = async () => {
-  const { data: {meals} } = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+  const { data: { meals } } = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=');
   return meals.slice(0, 12);
+};
+
+const getFirstDrinkRecipes = async () => {
+  const { data: { drinks } } = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+  return drinks.slice(0, 12);
 };
 
 export {
@@ -25,4 +30,5 @@ export {
     getFoodByName,
     getFoodByFirstName,
     getFirstFoodRecipes,
+    getFirstDrinkRecipes,
 };
