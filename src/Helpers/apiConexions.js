1bd9cf48366b5140e429cdf06fbea03ce33f23f0
cae <eduardokae01@hotmail.com>
2022-07-08 13:02:27 -0300

    feat: get first twelve recipes function

--- src/Helpers/apiConexions.js
+++ src/Helpers/apiConexions.js
@@ -2,24 +2,27 @@ import axios from 'axios';
 
 const getFoodByIngredient = async (ingredient) => {
   const { data: {meals} } = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
-
   return meals;
 };
 
 const getFoodByName = async (name) => {
   const { data: {meals} } = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
-
   return meals;
 };
 
 const getFoodByFirstName = async (firstName) => {
   const { data: {meals} } = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?f=${firstName}`);
-  console.log(meals);
   return meals;
+};
+
+const getFirstFoodRecipes = async () => {
+  const { data: {meals} } = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+  return meals.slice(0, 12);
 };
 
 export {
     getFoodByIngredient,
     getFoodByName,
-    getFoodByFirstName
+    getFoodByFirstName,
+    getFirstFoodRecipes,
 };
