9c8ee69000b0c6e21ade6b528d5f68d1bb3df4e8
cae <eduardokae01@hotmail.com>
2022-07-08 12:55:00 -0300

    style: change spaces from 4 to 2

--- src/Helpers/apiConexions.js
+++ src/Helpers/apiConexions.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 const getFoodByIngredient = async (ingredient) => {
-    const { data: {meals} } = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+  const { data: {meals} } = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
 
-    return meals;
+  return meals;
 };
 
 const getFoodByName = async (name) => {
-    const { data: {meals} } = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
+  const { data: {meals} } = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
 
-    return meals;
+  return meals;
 };
 
 const getFoodByFirstName = async (firstName) => {
-    const { data: {meals} } = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?f=${firstName}`);
-    console.log(meals);
-    return meals;
+  const { data: {meals} } = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?f=${firstName}`);
+  console.log(meals);
+  return meals;
 };
 
 export {
