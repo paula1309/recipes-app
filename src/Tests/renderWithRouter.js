bb18f389d93a89e46825a9ec3b47d1bcc80d0376
cae <eduardokae01@hotmail.com>
2022-07-08 23:05:25 -0300

    refactor: helper

--- src/Tests/renderWithRouter.js
+++ src/Tests/renderWithRouter.js
@@ -1,13 +1,10 @@
-import userEvent from "@testing-library/user-event";
-import { BrowserRouter, render } from "react-router-dom";
+import { render } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom";
 
 const renderWithRouter = (ui, {route = '/'} = {}) => {
   window.history.pushState({}, 'Test page', route)
 
-  return {
-    user: userEvent.setup(),
-    ...render(ui, {wrapper: BrowserRouter}),
-  }
-};
+  return render(ui, {wrapper: BrowserRouter})
+}
 
 export default renderWithRouter;
