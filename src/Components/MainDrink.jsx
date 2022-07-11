5d7c223af048953c388967e5131b4fd26b80e7f8
cae <eduardokae01@hotmail.com>
2022-07-11 12:11:25 -0300

    feat: add categories buttons

--- src/Components/MainDrink.jsx
+++ src/Components/MainDrink.jsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { getFirstDrinkRecipes } from '../Helpers/apiConexions';
+import { getDrinkCategories, getFirstDrinkRecipes } from '../Helpers/apiConexions';
 
 export default function MainDrink() {
   const [recipes, setRecipes] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     async function getFirstRecipes() {
       const data = await getFirstDrinkRecipes();
+      const categories = await getDrinkCategories();
+      setCategories(categories);
       setRecipes(data);
     }
     getFirstRecipes();
   }, []);
 
   return (
-    recipes.map((recipe) => {
-      return (
-        <section key={ recipe.strDrink }>
-          <img src={ recipe.strDrinkThumb } alt={ recipe.strDrink } width="125px" />
-          <h6>{ recipe.strDrink }</h6>
-        </section>
-      );
-    })
+    <>
+      {categories.map((category) => {
+        return (
+          <nav key={ category.strCategory }>
+            <button>
+              { category.strCategory }
+            </button>
+          </nav>
+        )
+      })}
+      {recipes.map((recipe) => {
+        return (
+          <section key={ recipe.idDrink }>
+            <img src={ recipe.strDrinkThumb } alt={ recipe.strDrink } width="125px" />
+            <h6>{ recipe.strDrink }</h6>
+          </section>
+        )
+      })}
+    </>
   );
 };
+
+
--- src/Components/MainFood.jsx
+++ src/Components/MainFood.jsx
@@ -1,28 +1,40 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { getFirstFoodRecipes } from "../Helpers/apiConexions";
+import { getFirstFoodRecipes, getFoodCategories } from "../Helpers/apiConexions";
 
 export default function MainFood() {
   const [recipes, setRecipes] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     async function getFoodRecipes() {
-      const data = await getFirstFoodRecipes();
-      setRecipes(data);
+      const recipes = await getFirstFoodRecipes();
+      const categories = await getFoodCategories();
+      setCategories(categories);
+      setRecipes(recipes);
     }
     getFoodRecipes();
   }, []);
 
-  console.log(recipes);
-
   return (
-    recipes.map((recipe) => {
-      return (
-        <section key={ recipe.idMeal }>
-          <img src={ recipe.strMealThumb } alt={ recipe.Meal } width="125px" />
-          <h6>{ recipe.strMeal }</h6>
-        </section>
-      )
-    })
+    <>
+      {categories.map((category) => {
+        return (
+          <nav key={ category.strCategory }>
+            <button>
+              { category.strCategory }
+            </button>
+          </nav>
+        )
+      })}
+      {recipes.map((recipe) => {
+        return (
+          <section key={ recipe.idMeal }>
+            <img src={ recipe.strMealThumb } alt={ recipe.Meal } width="125px" />
+            <h6>{ recipe.strMeal }</h6>
+          </section>
+        )
+      })}
+    </>
   )
 };
