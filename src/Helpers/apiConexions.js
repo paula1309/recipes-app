52c6a9725ffd7c91e335dba49269c472c50b05af
cae <eduardokae01@hotmail.com>
2022-07-11 12:10:51 -0300

    feat: getCategories

--- src/Helpers/apiConexions.js
+++ src/Helpers/apiConexions.js
@@ -25,10 +25,22 @@ const getFirstDrinkRecipes = async () => {
   return drinks.slice(0, 12);
 };
 
+const getFoodCategories = async () => {
+  const { data: { meals } } = await axios.get('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+  return meals.slice(0, 5);
+};
+
+const getDrinkCategories = async () => {
+  const { data: { drinks } } = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+  return drinks.slice(0, 5);
+};
+
 export {
     getFoodByIngredient,
     getFoodByName,
     getFoodByFirstName,
     getFirstFoodRecipes,
     getFirstDrinkRecipes,
+    getFoodCategories,
+    getDrinkCategories,
 };
